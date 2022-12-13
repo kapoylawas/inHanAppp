7211b42c6893463594b493630a3a0afcafee5b15
arief sangga <arif.sangga@gmail.com>
2022-12-13 18:11:17 +0700

    update warna

--- src/pages/web/AllMenu/index.js
+++ src/pages/web/AllMenu/index.js
@@ -2,6 +2,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import LayoutWeb from "../../../layouts/web";
+import { ReactComponent as Ppid } from "../../../assets/image/ic_ppid_by_design.svg";
+import { ReactComponent as Bliko } from "../../../assets/image/ic_blikosda_by_design.svg";
+import { ReactComponent as Ulpim } from "../../../assets/image/ic_ulpim_by_design.svg";
+import { ReactComponent as Silpu } from "../../../assets/image/ic_silpusitron_by_design.svg";
 
 function AllMenu() {
   return (
@@ -13,55 +17,39 @@ function AllMenu() {
               <div>
                 <div>
                   <div className="grid items-center grid-cols-4 gap-5 mt-5 text-center md:gap-5">
-                    <div className="col-span-2 p-4 text-xs text-center bg-red-200 rounded-md shadow-md md:col-span-1 lg:col-span-1">
+                    <div className="col-span-2 p-4 text-xs text-center bg-gray-100 rounded-md shadow-md md:col-span-1 lg:col-span-1">
                       <Link to="/web/ppid">
                         <div>
-                          <img
-                            src={require("../../../assets/img/ic_ppid.png")}
-                            width="30"
-                            className="inline-block mb-2"
-                          />
+                        <Ppid width="30" className="inline-block mb-2" />
                         </div>
                         PPID
                       </Link>
                     </div>
-                    <div className="col-span-2 p-4 text-xs text-center bg-red-200 rounded-md shadow-md md:col-span-1 lg:col-span-1">
+                    <div className="col-span-2 p-4 text-xs text-center bg-gray-100 rounded-md shadow-md md:col-span-1 lg:col-span-1">
                       <Link to="/web/blikosda">
                         <div>
-                          <img
-                            src={require("../../../assets/img/ic_blikosda – 1.png")}
-                            width="30"
-                            className="inline-block mb-2"
-                          />
+                        <Bliko width="30" className="inline-block mb-2" />
                         </div>
                         Blikosda
                       </Link>
                     </div>
-                    <div className="col-span-2 p-4 text-xs text-center bg-red-200 rounded-md shadow-md md:col-span-1 lg:col-span-1">
+                    <div className="col-span-2 p-4 text-xs text-center bg-gray-100 rounded-md shadow-md md:col-span-1 lg:col-span-1">
                       <Link to="/web/ulpim">
                         <div>
-                          <img
-                            src={require("../../../assets/img/ic_ulpim.png")}
-                            width="30"
-                            className="inline-block mb-2"
-                          />
+                        <Ulpim width="30" className="inline-block mb-2" />
                         </div>
                         Ulpim
                       </Link>
                     </div>
-                    <div className="col-span-2 p-4 text-xs text-center bg-red-200 rounded-md shadow-md md:col-span-1 lg:col-span-1">
+                    <div className="col-span-2 p-4 text-xs text-center bg-gray-100 rounded-md shadow-md md:col-span-1 lg:col-span-1">
                       <Link to="/web/sipul">
                         <div>
-                          <img
-                            src={require("../../../assets/img/ic_silpulstron.png")}
-                            width="30"
-                            className="inline-block mb-2"
-                          />
+                        <Silpu width="30" className="inline-block mb-2" />
                         </div>
                         Silpulsitron
                       </Link>
                     </div>
-                    <div className="col-span-2 p-4 text-xs text-center bg-red-200 rounded-md shadow-md md:col-span-1 lg:col-span-1">
+                    <div className="col-span-2 p-4 text-xs text-center bg-gray-100 rounded-md shadow-md md:col-span-1 lg:col-span-1">
                       <Link to="/web/radio">
                         <div>
                           <img
@@ -73,7 +61,7 @@ function AllMenu() {
                         Radio
                       </Link>
                     </div>
-                    <div className="col-span-2 p-4 text-xs text-center bg-red-200 rounded-md shadow-md md:col-span-1 lg:col-span-1">
+                    <div className="col-span-2 p-4 text-xs text-center bg-gray-100 rounded-md shadow-md md:col-span-1 lg:col-span-1">
                       <Link to="/web/webview">
                         <div>
                           <img
@@ -85,7 +73,7 @@ function AllMenu() {
                         Web View
                       </Link>
                     </div>
-                    <div className="col-span-2 p-4 text-xs text-center bg-red-200 rounded-md shadow-md md:col-span-1 lg:col-span-1">
+                    <div className="col-span-2 p-4 text-xs text-center bg-gray-100 rounded-md shadow-md md:col-span-1 lg:col-span-1">
                       <Link to="/web/callCenter">
                         <div>
                           <img
